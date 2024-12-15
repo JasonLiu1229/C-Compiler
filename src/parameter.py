597a8591b35e9e0ffc72e8ef9aa63ddecba54af7
Orfeo Terkuci <terkuciorfeo@gmail.com>
2024-12-15 17:47:40 +0100

    Format files

--- src/parameter.py
+++ src/parameter.py
@@ -16,7 +16,11 @@ class FunctionParameter:
     def __eq__(self, o: object) -> bool:
         if not isinstance(o, FunctionParameter):
             return False
-        return self.type == o.type and self.name == o.name and self.default_value == o.default_value
+        return (
+            self.type == o.type
+            and self.name == o.name
+            and self.default_value == o.default_value
+        )
 
     def __ne__(self, o: object) -> bool:
         return not self.__eq__(o)
